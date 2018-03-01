451fd167f9dd3e81453eea84c91f1496555321ae
moshe <kottamasi.moshe@votarytech.com>
2018-03-01 13:18:22 +0530

    Data on&off

---
+++ Data_ON_OFF.py
@@ -0,0 +1,33 @@
+import os,time,datetime
+def collect_logs():
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.makedirs(mydir)
+	cmdr='adb logcat -b radio  > sms_log'+'&'
+	os.system(cmdr)
+def Data_enable(div):
+		cmd="adb -s "+div+" shell svc data enable"
+		rc=os.system(cmd)
+		print "mobile data Enabled" 
+		time.sleep(5)
+		return rc
+def Data_disable(div):
+		cmd="adb -s "+div+" shell svc data disable"
+		rc=os.system(cmd)
+		print "mobile data Disable"
+		time.sleep(5)
+		return rc
+
+def iter_status(ite,div):
+	collect_logs()
+	for i in range(ite):
+		on= Data_enable(div)
+		off=Data_disable(div)
+		if on==off:
+			print "Test passed"
+		else:
+			print "Test Failed"
+
+
+div = "HQ541YL17255"
+iterations=3
+iter_status(iterations,div)

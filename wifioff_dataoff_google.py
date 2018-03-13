5e9056b8f7c1e8d74f42dfbe6a74daa7163d0025
saiprakashthangala <36496738+saiprakashthangala@users.noreply.github.com>
2018-03-13 12:29:46 +0530

    Add files via upload

---
+++ wifioff_dataoff_google.py
@@ -0,0 +1,37 @@
+import os,time,datetime
+
+def collect_logs():
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.makedirs(mydir)
+	cmdr='adb logcat -b radio  > sms_log'+'&'
+	os.system(cmdr)
+def wifi_off():
+             cmd= "adb -s "+div+" shell svc wifi disable"
+             rc=os.system(cmd)
+             print "wifii disabled"
+             time.sleep(5)
+             return rc
+def data_off(div):
+             cmd= "adb -s "+div+" shell svc data disable"
+             rc=os.system(cmd)
+             print "data disabled"
+             time.sleep(10)
+             return rc
+def open_browse():
+             cmd= "adb -s "+div+" shell am start -a android.intent.action.VIEW -d http://google.com"
+             rc=os.system(cmd)
+             print "google opened"
+             time.sleep(20)
+             return rc
+
+
+def iter_status(iterations,div):
+        collect_logs()
+        for i in range(iterations):
+                print i+1
+                off=wifi_off()
+                off=data_off(div)
+                open1=open_browse()
+div = "ZX1D64GJW6"
+iterations=3
+iter_status(iterations,div)
---
+++ wifioff_dataoff_youtube.py
@@ -0,0 +1,35 @@
+import os,time,datetime
+
+def collect_logs():
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.makedirs(mydir)
+	cmdr='adb logcat -b radio  > sms_log'+'&'
+	os.system(cmdr)
+def wifi_off():
+             cmd= "adb -s "+div+" shell svc wifi disable"
+             rc=os.system(cmd)
+             print "wifii disabled"
+             time.sleep(5)
+             return rc
+def data_off(div):
+             cmd= "adb -s "+div+" shell svc data disable"
+             rc=os.system(cmd)
+             print "data disabled"
+             time.sleep(10)
+             return rc
+def youtube():
+             cmd= "adb -s "+div+" shell am start -a android.intent.action.VIEW -d http://www.youtube.com/watch?v=xko3vld5Wds"
+             rc=os.system(cmd)
+             print "youtube not opened"
+             time.sleep(10)
+             return rc
+def iter_status(iterations,div):
+        collect_logs()
+        for i in range(iterations):
+                print i+1
+                off=wifi_off()
+                off=data_off(div)
+                play=youtube()
+div = "ZX1D64GJW6"
+iterations=3
+iter_status(iterations,div)

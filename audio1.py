f1b9b14f7796c9d05abc838187fc68c102da8ae9
saiprakashthangala <36496738+saiprakashthangala@users.noreply.github.com>
2018-03-14 15:56:04 +0530

    Add files via upload

---
+++ audio1.py
@@ -0,0 +1,27 @@
+import os,time,datetime
+
+def collect_logs():
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.makedirs(mydir)
+	cmdr='adb logcat -b radio  > sms_log'+'&'
+	os.system(cmdr)
+def play_audio():
+		cmd= "adb shell am start -a android.intent.action.VIEW -d file:/storage/emulated/0/Music/sound.mp3 -t audio/mp3"
+		rc=os.system(cmd)
+                time.sleep(7)
+                cmd1="adb shell input keyevent 127"
+                rc=os.system(cmd1)
+		print "audio played" 
+		time.sleep(10)
+                return rc
+
+def iter_status(iterations,div):
+        collect_logs()
+        for i in range(iterations):
+                play=play_audio()
+                print i+1
+                
+div = "ZX1D64GJW6"
+iterations=3
+iter_status(iterations,div)
+
---
+++ audio_call.py
@@ -0,0 +1,29 @@
+import os,time,datetime
+def collect_logs():
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.makedirs(mydir)
+	cmdr='adb logcat -b radio  > sms_log'+'&'
+	os.system(cmdr)
+def call(div,num):
+		cmd1 ="adb -s "+div+" shell am start -a android.intent.action.CALL -d tel:"+num+""
+		rc = os.system(cmd1)
+                return rc
+def play_audio():
+		cmd= "adb shell am start -a android.intent.action.VIEW -d file:/storage/emulated/0/Music/sound.mp3 -t audio/mp3"
+		rc=os.system(cmd)
+                time.sleep(10)
+		print "audio played" 
+		time.sleep(7)
+                return rc
+
+def iter_status(iterations,div):
+        collect_logs()
+        for i in range(iterations):
+                play=play_audio()
+                call_on = call(div,num) 
+                print i+1
+                
+div = "ZX1D64GJW6"
+num = "+919000137251"
+iterations=3
+iter_status(iterations,div)

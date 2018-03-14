a0b52dd61a4c1b09aead6e0731dacbb86acfe3d0
saiprakashthangala <36496738+saiprakashthangala@users.noreply.github.com>
2018-03-14 16:25:06 +0530

    Add files via upload

---
+++ call_audio_end.py
@@ -0,0 +1,36 @@
+import os,time,datetime
+
+def collect_logs():
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	os.makedirs(mydir)
+	cmdr='adb logcat -b radio  > sms_log'+'&'
+	os.system(cmdr)
+def call(div,num):
+		cmd1 ="adb -s "+div+" shell am start -a android.intent.action.CALL -d tel:"+num+""
+		rc = os.system(cmd1)
+                return rc
+def play_audio():
+		cmd= "adb shell am start -a android.intent.action.VIEW -d file:/storage/emulated/0/Music/sound.mp3 -t audio/mp3"
+		rc=os.system(cmd)
+                time.sleep(7)
+		print "audio played" 
+                return rc
+def endcall():
+                cmd2 = "adb shell input  keyevent 6"
+                rc=os.system(cmd2)
+                time.sleep(5)
+                return rc
+               
+def iter_status(iterations,div):
+        collect_logs()
+        for i in range(iterations):
+                call_on = call(div,num)
+                play=play_audio()
+                end=endcall()
+                play1=play_audio()   
+                print i+1
+                
+div = "ZX1D64GJW6"
+num = "+919000137251"
+iterations=3
+iter_status(iterations,div)

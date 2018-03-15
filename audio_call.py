a308e387f7500a382d1f0fdd30a233f818ab373f
saiprakashthangala <36496738+saiprakashthangala@users.noreply.github.com>
2018-03-15 10:51:00 +0530

    Update audio_call.py

--- audio_call.py
+++ audio_call.py
@@ -1,8 +1,8 @@
 import os,time,datetime
 def collect_logs():
-	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('Audio call_%Y-%m-%d_%H-%M-%S'))
 	os.makedirs(mydir)
-	cmdr='adb logcat -b radio  > sms_log'+'&'
+	cmdr='adb logcat -b event > sms_log'+'&'
 	os.system(cmdr)
 def call(div,num):
 		cmd1 ="adb -s "+div+" shell am start -a android.intent.action.CALL -d tel:"+num+""
@@ -11,9 +11,9 @@ def call(div,num):
 def play_audio():
 		cmd= "adb shell am start -a android.intent.action.VIEW -d file:/storage/emulated/0/Music/sound.mp3 -t audio/mp3"
 		rc=os.system(cmd)
-                time.sleep(10)
-		print "audio played" 
-		time.sleep(7)
+                time.sleep(5)
+		print "audio played"
+		time.sleep(5)
                 return rc
 
 def iter_status(iterations,div):

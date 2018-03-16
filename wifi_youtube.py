116862f95e81e5a8ee5068543e15b103b54a229c
saiprakashthangala <36496738+saiprakashthangala@users.noreply.github.com>
2018-03-16 13:47:52 +0530

    Update wifi_youtube.py

--- wifi_youtube.py
+++ wifi_youtube.py
@@ -1,10 +1,12 @@
 import os,time,datetime
 
 def collect_logs():
-	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime(wifi_youtube_'%Y-%m-%d_%H-%M-%S'))
 	os.makedirs(mydir)
-	cmdr='adb logcat -b radio  > sms_log'+'&'
+	cmdr='adb logcat -b radio  > radio_log'+'&'
 	os.system(cmdr)
+	cmde='adb logcat -b event  > event_log'+'&'
+	os.system(cmde)
 
 def wifi_on():
              cmd= "adb -s "+div+" shell svc wifi enable"

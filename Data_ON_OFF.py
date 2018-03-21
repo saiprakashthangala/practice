fb045baf330c62d3dc1099ece995c71c56df3539
moshe <kottamasi.moshe@votarytech.com>
2018-03-21 12:37:27 +0530

    add home page

--- Data_ON_OFF.py
+++ Data_ON_OFF.py
@@ -1,23 +1,34 @@
-import os,time,datetime
+import os,time,datetime,csv
+import device
 def collect_logs():
 	mydir = os.path.join(os.getcwd(), datetime.datetime.now().strftime('Data_ON_OFF_%Y-%m-%d_%H-%M-%S'))
 	os.makedirs(mydir)
 	os.chdir(mydir)
 	cmdr='adb logcat -b > Data_log'+'&'
 	os.system(cmdr)
-def Data_enable(div):
-		cmd="adb -s "+div+" shell svc data enable"
-		rc=os.system(cmd)
-		print "mobile data Enabled" 
-		time.sleep(5)
-		return rc
-def Data_disable(div):
-		cmd="adb -s "+div+" shell svc data disable"
-		rc=os.system(cmd)
-		print "mobile data Disable"
-		time.sleep(5)
-		return rc
+def iterations():
+	f=open('Mo_num_msg.csv', 'r')
+	reader = csv.reader(f)
+	your_list = list(reader)
+	iters=your_list[2]
+	return int(random.choice(iters[1::]))
 
+def Data_enable(div):
+	cmd="adb shell input keyevent 3"
+	os.system(cmd)			#set home page 
+	cmd1="adb -s "+div+" shell svc data enable"
+	rc=os.system(cmd1)
+	print "mobile data Enabled" 
+	time.sleep(5)
+	return rc
+def Data_disable(div):
+	cmd="adb shell input keyevent 3"
+	os.system(cmd)			#set home page
+	cmd1="adb -s "+div+" shell svc data disable"
+	rc=os.system(cmd1)
+	print "mobile data Disable"
+	time.sleep(5)
+	return rc
 def iter_status(ite,div):
 	collect_logs()
 	for i in range(ite):
@@ -29,6 +40,6 @@ def iter_status(ite,div):
 			print "Test Failed"
 
 
-div = "HQ541YL17255"
-iterations=3
+div =device.main()
+iterations=iterations()
 iter_status(iterations,div)
